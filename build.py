18119859d8e62d6daa6425ba24ea6c1420fd19f7
coded9 <coded9@users.noreply.github.com>
2017-06-11 13:33:08 +0000

    Done

---
+++ build.py
@@ -0,0 +1,12 @@
+def two_sum(arr,sum):
+    arrDict = {}
+    if arr is None:
+        raise TypeError
+    elif len(arr) == 0:
+        raise ValueError
+    else:
+        for i in range(0,len(arr)):
+            arrDict[arr[i]] = arr[i]
+            if sum-arr[i] in arrDict:
+                return list([arr.index(sum-arr[i]),i])
+print two_sum([1, 3, 2, -7, 5],7)
